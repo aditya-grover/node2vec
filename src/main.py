3c81d87e3ebaece5eafdb0ab509f21166ee7c24e
Aditya Grover <aditya@DNab40467c.stanford.edu>
2016-06-27 19:44:54 -0700

    initial codebase

---
+++ src/main.py
@@ -0,0 +1,86 @@
+'''
+
+
+
+'''
+
+
+import argparse
+import numpy as np
+import networkx as nx
+import node2vec
+
+def parse_args():
+	'''
+	Parses the node2vec arguments.
+	'''
+	parser = argparse.ArgumentParser(description="Run node2vec.")
+
+	parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
+	                    help='Input graph path')
+
+	parser.add_argument('--save-path', nargs='?', default='emb/karate.emb',
+	                    help='Embeddings path')
+
+	parser.add_argument('--dimensions', type=int, default=128,
+	                    help='Number of dimensions. Default is 128.')
+
+	parser.add_argument('--walk-length', type=int, default=10,
+	                    help='Length of walk per source. Default is 10.')
+
+	parser.add_argument('--num-walks', type=int, default=10,
+	                    help='Number of walks per source. Default is 40.')
+
+	parser.add_argument('--p', type=float, default=1,
+	                    help='Return hyperparameter. Default is 1.')
+
+	parser.add_argument('--q', type=float, default=1,
+	                    help='Inout hyperparameter. Default is 1.')
+
+	parser.add_argument('--weighted', dest='weighted', action='store_true',
+	                    help="To use perplexity as a measure")
+	parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+	parser.set_defaults(weighted=False)
+
+	parser.add_argument('--directed', dest='directed', action='store_true',
+	                    help="To use perplexity as a measure")
+	parser.add_argument('--undirected', dest='undirected', action='store_false')
+	parser.set_defaults(directed=False)
+
+	return parser.parse_args()
+
+def read_graph():
+	'''
+	Reads the input network in networkx.
+	'''
+	if args.weighted:
+		G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
+	else:
+		G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
+		G = nx.barbell_graph(2,2)
+		for edge in G.edges():
+			G[edge[0]][edge[1]]['weight'] = 1
+
+	if not args.directed:
+		G = G.to_undirected()
+
+	return G
+
+def learn_embeddings(walks):
+
+
+	return
+
+def main(args):
+	nx_G = read_graph()
+	G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
+	G.preprocess_transition_probs()
+	walks = G.simulate_walks(args.num_walks, args.walk_length)
+	learn_embeddings(walks)
+
+args = parse_args()
+main(args)
+
+
+
+
