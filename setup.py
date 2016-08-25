35b5d60e28d351d37bf23ee9efe9241089271b43
poldrack <poldrack@gmail.com>
2016-08-25 14:59:05 -0700

    added exception for python 3

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ descr = """node2vec: algorithm for learning continuous representations for nodes
 
 import os
 from setuptools import setup
+from sys import version
+
+if version > '2.8.0':
+    raise Exception('Currently only works in Python 2.7')
 
 DISTNAME="node2vec"
 DESCRIPTION=descr
@@ -17,35 +21,12 @@ URL='http://snap.stanford.edu/node2vec/'
 DOWNLOAD_URL='https://github.com/aditya-grover/node2vec'
 VERSION='0.1'
 
-def check_dependencies():
-
-    # Just make sure dependencies exist, I haven't rigorously
-    # tested what the minimal versions that will work are
-    needed_deps = ["gensim", "numpy", "networkx"]
-    missing_deps = []
-    for dep in needed_deps:
-        try:
-            __import__(dep)
-        except ImportError:
-            missing_deps.append(dep)
-
-    if missing_deps:
-        missing = (", ".join(missing_deps)
-                   .replace("sklearn", "scikit-learn"))
-        raise ImportError("Missing dependencies: %s" % missing)
-
 if __name__ == "__main__":
 
     if os.path.exists('MANIFEST'):
         os.remove('MANIFEST')
 
     import sys
-    if not (len(sys.argv) >= 2 and ('--help' in sys.argv[1:] or
-            sys.argv[1] in ('--help-commands',
-                            '--version',
-                            'egg_info',
-                            'clean'))):
-        check_dependencies()
 
     setup(name=DISTNAME,
         maintainer=MAINTAINER,
@@ -56,6 +37,7 @@ if __name__ == "__main__":
         version=VERSION,
         url=URL,
         download_url=DOWNLOAD_URL,
+	install_requires=['gensim','networkx'],
         packages=['node2vec'],
         scripts=['scripts/run_node2vec.py'],
         classifiers=[
