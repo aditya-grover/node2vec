25af18b37e10c8e858529f49de39fc30cf46ae2e
Yoel Zeldes <yoel.zeldes@gmail.com>
2018-10-16 23:30:46 +0300

    bug fix: model.save_word2vec_format is deprecated

--- src/main.py
+++ src/main.py
@@ -85,7 +85,7 @@ def learn_embeddings(walks):
 	'''
 	walks = [map(str, walk) for walk in walks]
 	model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
-	model.save_word2vec_format(args.output)
+	model.wv.save_word2vec_format(args.output)
 	
 	return
 
