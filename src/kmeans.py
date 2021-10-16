5efde2f2efc2d70daa68e44dec8222cc70798ee0
Dmitrijs Kass <dmitrijs.kass@gmail.com>
2021-10-16 15:45:03 +0200

    Now other external edgelist datasets can be processed.

--- src/kmeans.py
+++ src/kmeans.py
@@ -4,15 +4,28 @@ import networkx as nx
 from sklearn.cluster import KMeans
 from matplotlib import cm
 
+SWITCH = "str_eq" # homophily, str_eq, struc2vec
 
-SWITCH = "homophily" # homophily, str_eq, struc2vec
-args = {"EDGELIST_FNAME": "graph/les_miserables_int.edgelist",
+DATA_NAME = "les_miserables" # les_miserables, TerroristRel
+# TerroristRel: https://networkrepository.com/TerroristRel.php
+
+args = {"edgelist_fname": f"graph/{DATA_NAME}.edgelist",
         "D": 16, 
         "P": 1,
         "K": 10, # window_size = context size k, default = 10.
         "L": 80 # walk_length = l, default = 80.
         }
 
+def get_data(data_name):
+    """Get data as a networkx graph."""
+    
+    if data_name == "les_miserables":
+        return nx.generators.social.les_miserables_graph()
+    elif data_name == "TerroristRel":
+        return nx.readwrite.edgelist.read_edgelist("graph/TerroristRel.edges", 
+                                                   delimiter = ",")
+    else:
+        raise Exception("Unknown dataset.")
 
 def set_other_parameters():
     """Set q and the number of clusters as in the original paper,
@@ -35,7 +48,7 @@ def set_other_parameters():
     elif SWITCH == "struc2vec":
         print("Using for structural equivalence with struc2vec...")
         args["n_clusters"] = 4
-        args["emb_fname"] = "../BioNEV/embeddings/les_miserables_struc2vec.emb"
+        args["emb_fname"] = f"../BioNEV/embeddings/{DATA_NAME}_struc2vec.emb"
         args["plot_suffix"] = "str_eq"
         args["params"] = "struc2vec"
         
@@ -57,58 +70,28 @@ def save_fig(filename, h, w, dpi):
     figure = plt.gcf()
     figure.set_size_inches(w, h)
     plt.savefig(filename, dpi=dpi)
-
-def plot_elbow_method():
-    """Plot and save an elbow method for kmeans."""
-    
-    # Source: https://stackoverflow.com/questions/19197715/scikit-learn-k-means-elbow-criterion
-    sse = {}
-    for k in range(1, 10):
-        kmeans = KMeans(n_clusters=k, max_iter=1000).fit(emb_lst)
-        sse[k] = kmeans.inertia_ # Inertia: Sum of distances of samples to their closest cluster center
-    plt.figure()
-    plt.plot(list(sse.keys()), list(sse.values()))
-    plt.xlabel("Number of clusters")
-    plt.ylabel("SSE")
-    save_fig("images/les_miserables/elbow.png", h = 5, w = 7, dpi = 200)
-    plt.show()
-
-def plot_original_network(G):
-    """Plot the network and export into a file"""
-    
-    # Get node degrees for the plot.
-    d = dict(G.degree)
-    step = int(max(d.values()) / 5) # Discretize node degrees into 5 groups (~ as in Figure 3).
-    node_size = [int(e / step + 1) * 200 + 150 for e in d.values()]
-    
-    # Plot the network.
-    subax1 = plt.subplot()
-    nx.draw(G, with_labels=True, node_size=node_size)
-    save_fig("images/les_miserables/les_miserables_draw.png", h = 10, w = 20, dpi = 200)
-    plt.show()
-    
-    return node_size
-
+   
 def export_edgelist(G):
     """Export G as an edgelist with integer IDs."""
     
-    G_int = nx.convert_node_labels_to_integers(G, first_label = 0)
-    nx.readwrite.edgelist.write_edgelist(G = G_int, path = args["EDGELIST_FNAME"], data = False)
+    if DATA_NAME == "les_miserables":
+        # Because we took it from networkx package.
+        G_int = nx.convert_node_labels_to_integers(G, first_label = 0)
+        nx.readwrite.edgelist.write_edgelist(G = G_int, path = args["edgelist_fname"], data = False)
 
 def create_embeddings():
     """Execute node2vec or struc2vec to create embeddings."""
 
-    # Execute node2vec -------------------------------------
     if SWITCH == "struc2vec":
         # Install BioNEV package according to the instructions in the README: 
             # https://github.com/xiangyue9607/BioNEV.
         # Run 
             # cd ../BioNEV
-            # bionev --input ../node2vec/graph/les_miserables_int.edgelist --output ./embeddings/les_miserables_struc2vec.emb --method struc2vec --task link-prediction --walk-length 80 --window-size 10 --dimensions 16
+            # bionev --input ../node2vec/graph/les_miserables.edgelist --output ./embeddings/les_miserables_struc2vec.emb --method struc2vec --task link-prediction --walk-length 80 --window-size 10 --dimensions 16
         pass
     else:
         cmd = "python src/main.py " + \
-            "--input " + args["EDGELIST_FNAME"] + \
+            "--input " + args["edgelist_fname"] + \
             " --output " + args["emb_fname"] + \
             " --dimensions " + str(args["D"]) + \
             " --walk-length " + str(args["L"]) + \
@@ -139,14 +122,33 @@ def do_clustering(emb_lst):
 
     kmeans = KMeans(n_clusters=args["n_clusters"], random_state=0).fit(emb_lst)
     kmeans.labels_
-    
-    if SWITCH == "struc2vec":
-        # Plot and export an image with an elbow method for kmeans.
-        plot_elbow_method()
         
     return kmeans
 
-def plot_network_with_clusters(emb, kmeans, node_names, node_size):
+def plot_elbow_method(emb_lst):
+    """Plot and save an elbow method for kmeans."""
+    
+    # Source: https://stackoverflow.com/questions/19197715/scikit-learn-k-means-elbow-criterion
+    sse = {}
+    for k in range(1, 30):
+        kmeans = KMeans(n_clusters=k, max_iter=1000).fit(emb_lst)
+        sse[k] = kmeans.inertia_ # Inertia: Sum of distances of samples to their closest cluster center
+    plt.figure()
+    plt.plot(list(sse.keys()), list(sse.values()))
+    plt.xlabel("Number of clusters")
+    plt.ylabel("SSE")
+    save_fig(f"images/{DATA_NAME}/elbow.png", h = 5, w = 7, dpi = 200)
+    plt.show()
+
+def plot_original_network(G):
+    """Plot the network and export into a file"""
+    
+    subax1 = plt.subplot()
+    nx.draw(G, with_labels=True, alpha = 0.7)
+    save_fig(f"images/{DATA_NAME}/{DATA_NAME}_draw.png", h = 10, w = 20, dpi = 200)
+    plt.show()
+
+def plot_network_with_clusters(emb, kmeans, node_names):
     """Plot the original graph with colours according to the clusters."""
 
     node_keys = list(emb.keys())
@@ -160,21 +162,23 @@ def plot_network_with_clusters(emb, kmeans, node_names, node_size):
         cluster = node_clusters[name_index]
         color_map.append(cmap(cluster))
      
-    nx.draw(G, node_color=color_map, with_labels=True, node_size=node_size)
-    save_fig(f"images/les_miserables/les_miserables_{args['params']}_draw_kmeans_{args['plot_suffix']}.png", h = 10, w = 20, dpi = 200)
+    nx.draw(G, node_color=color_map,
+            with_labels=True, # CHANGE HERE TO ADD LABELS!
+            alpha = 0.7)
+    save_fig(f"images/{DATA_NAME}/{DATA_NAME}_{args['params']}_kmeans_{args['n_clusters']}_clusters_{args['plot_suffix']}.png", 
+             h = 6, 
+             w = 10, 
+             dpi = 200)
     plt.show()
 
 
-
-    
-    
-
 if __name__ == "__main__":
     set_other_parameters()
-    G = nx.generators.social.les_miserables_graph()
-    node_size = plot_original_network(G)
+    G = get_data(DATA_NAME)
+    plot_original_network(G)
     export_edgelist(G)
     create_embeddings()
     emb, emb_lst, node_names = read_embeddings(G)
     kmeans = do_clustering(emb_lst)
-    plot_network_with_clusters(emb, kmeans, node_names, node_size)
+    plot_elbow_method(emb_lst)
+    plot_network_with_clusters(emb, kmeans, node_names)
