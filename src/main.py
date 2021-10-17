3527958c11c46ea909483c922d4a1e760e1496e8
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2021-10-17 11:58:57 +0200

    fix: add learn_embeddings fixes from python3.7 branch

--- src/main.py
+++ src/main.py
@@ -107,15 +107,15 @@ def read_graph():
     return  G
 
 def learn_embeddings(walks):
-	'''
-	Learn embeddings by optimizing the Skipgram objective using SGD.
-	'''
+    '''
+    Learn embeddings by optimizing the Skipgram objective using SGD.
+    '''
     # source: https://stackoverflow.com/questions/42836992/gensim-1-0-1-on-python-3-5-typeerror-object-of-type-map-has-no-len
-	walks = [list(map(str, walk)) for walk in walks] # convert each vertex id to a string
-	model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
-	model.wv.save_word2vec_format(args.output)
-	
-	return
+    walks = [list(map(str, walk)) for walk in walks] # convert each vertex id to a string
+    model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers, iter=args.iter)
+    model.wv.save_word2vec_format(args.output)
+
+    return
 
 def main(args):
     '''
